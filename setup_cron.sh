95010e48367c9cc5f7dffcffc6116dd1ee3f7cfc
Aron-Pascal Schneider <aron.schneider@pexon-consulting.de>
2023-06-09 16:52:28 +0000

    Delete setup_cron.sh

--- setup_cron.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "$CRON_SCHEDULE root python /app/automation.py >> /var/log/cron.log 2>&1" > /etc/cron.d/mycron
-
-chmod 0644 /etc/cron.d/mycron
-
-cron #&& tail -f /var/log/cron.log
