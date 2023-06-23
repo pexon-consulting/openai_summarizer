dc640543080d085a5c1a3d4721e27331ead89540
Christopher Rücker <christopher.ruecker@pexon-consulting.de>
2023-06-23 13:01:29 +0200

    Add logging to openai client

--- openai_summarizer.py
+++ openai_summarizer.py
@@ -2,6 +2,8 @@ import openai
 import logging
 import sys
 
+logging.getLogger(__name__)
+
 
 class OpenaiClient:
     def __init__(self, openai_api_key):
@@ -10,6 +12,8 @@ class OpenaiClient:
 
     def generate_summary_confluence(self, statement: str, text: str) -> str:
         prompt = f"{statement} {text}"
+
+        logging.info("Started openAI summary request")
         response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=prompt,
@@ -19,11 +23,11 @@ class OpenaiClient:
             temperature=0.3,
         )
 
+        logging.info("openAI summary request done")
+
         if "choices" in response and len(response["choices"]) > 0:
             summary = response["choices"][0]["text"].strip()
             return summary
         else:
             logging.error(f"Sending to OpenAI has failed. (╯°□°）╯︵ ┻━┻")
             sys.exit(1)
-
-        return None
